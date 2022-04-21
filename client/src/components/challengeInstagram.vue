e2cc320c5a8d3894b2487559dc1e1d2bb8104595
agustin-escobar97 <agustin.escobar97@gmail.com>
2022-04-21 14:27:35 -0300

    se agregan likes en tiempo real thx caro

--- client/src/components/challengeInstagram.vue
+++ client/src/components/challengeInstagram.vue
@@ -11,13 +11,16 @@
      :image="publicacion.image"
      :text="publicacion.text"
      :likes="publicacion.likes"
+     v-on:like="like"
      >
 
      </publicacionesInsta>
      </div>
 
+    <subirPublicacion
+
+      ></subirPublicacion>
   </div>
-<subirPublicacion></subirPublicacion>
 </template>
 
 <script>
@@ -41,8 +44,16 @@ export default {
   },
   methods:{
     obtenerPublicaciones(){
-      axios.get("http://localhost:3000/api/publicaciones").then((respuesta)=>this.publicaciones=respuesta.data)
-    }
+      axios.get("http://localhost:3000/api/publicaciones").then((respuesta)=>{
+        this.publicaciones = respuesta.data
+      })
+    },
+    like(id) {
+      axios.put("http://localhost:3000/api/publicaciones/darLike/" + id).then((res)=>{
+        this.publicaciones[id-1].likes = res.data.likes
+        console.log(this.publicaciones[id-1])
+      })
+   }
   },
   mounted(){
     this.obtenerPublicaciones()
--- client/src/components/publicacionesInsta.vue
+++ client/src/components/publicacionesInsta.vue
@@ -1,32 +1,21 @@
 <template>
-<!--Template para visualizar las publicaciones-->
-  <div></div>
   <div class="publicacionesEstilo">
+    <!--Template para visualizar las publicaciones-->
     <img :src="image" class="publicacionFoto"/>
     <p>{{ text }}</p>
-    <p><font-awesome-icon :icon="['fas', 'heart']" />{{ likes }}</p>
-    <button v-on:click="darLike(id)">Me gusta</button>
+    <a class="anchorLike" href="javascript:void(0);" v-on:click="this.$emit('like',id)"><font-awesome-icon :icon="['fas', 'heart']" /></a>{{ likes }}
     <hr/>
   </div>
-  <div></div>
 </template>
 
 <script>
-import axios from "axios";
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
   name: "publicacionesInsta",
   props: ["id", "image", "text", "likes"],
   data(){
     return{
-      updatedLikes:{}
   }},
-  methods: {
-    darLike(id) {
-      axios.put("http://localhost:3000/api/publicaciones/darLike/" + id)
-      console.log(axios.get("http://localhost:3000/api/publicaciones/" + id))
-    }
-    }
 };
 </script>
 
